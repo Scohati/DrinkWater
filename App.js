b703a59505291ff84c06f079e303ca9a54cbaf74
Diogo Scohati <diogoscohati@outlook.com>
2023-06-06 10:27:21 -0300

    feat: UI Kitten

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import * as eva from '@eva-design/eva';
+import { evaTheme } from './eva';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ApplicationProvider {...eva} theme={evaTheme} customMapping={{}}>
+      <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Welcome to your UI Kitten App!</Text>
+        <StatusBar style="auto" />
+      </Layout>
+    </ApplicationProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ eva.js
@@ -0,0 +1,6 @@
+import { mapping, light as lightTheme } from '@eva-design/eva';
+
+export const evaTheme = {
+  ...lightTheme,
+  'color-primary-500': '#3366FF',
+};
